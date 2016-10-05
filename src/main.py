91a9efcd7dcb482863e519d4ddf83629f8611d28
jsnyde0 <jonatansnyders@gmail.com>
2016-10-05 18:50:55 +0200

    VTK Read and Write test

--- src/main.py
+++ src/main.py
@@ -1,7 +1,56 @@
-# Here goes the magic
-# If the following 2 lines succeed it means that VTK is successfully installed
 import vtk
-print(vtk.__file__)
+import numpy as np
+from vtk.util.numpy_support import vtk_to_numpy
 
-print("Hello Jonatan ...")
-print("Is het antwoord op deze vraag negatief?")
+
+inputfilename = "/src/data/paris/558/skin95.stl"
+outputfilename = "/out/skin95out.stl"
+
+########
+# READ #
+########
+
+reader = vtk.vtkSTLReader()
+reader.SetFileName(inputfilename)
+
+##########
+# RENDER #
+##########
+
+mapper = vtk.vtkPolyDataMapper()
+mapper.SetInputConnection(reader.GetOutputPort())
+
+actor = vtk.vtkActor()
+actor.SetMapper(mapper)
+
+# Create a rendering window and renderer
+ren = vtk.vtkRenderer()
+renWin = vtk.vtkRenderWindow()
+renWin.AddRenderer(ren)
+
+# Create a renderwindowinteractor
+iren = vtk.vtkRenderWindowInteractor()
+iren.SetRenderWindow(renWin)
+
+# Assign actor to the renderer
+ren.AddActor(actor)
+
+# Enable user interface interactor
+iren.Initialize()
+renWin.Render()
+iren.Start()
+
+################
+# VTK TO NUMPY #
+################
+#nodes_vtk_array= reader.GetOutput().GetPoints().GetData()
+#nodes_nummpy_array = vtk_to_numpy(nodes_vtk_array)
+
+
+#########
+# WRITE #
+#########
+stlWriter = vtk.vtkSTLWriter()
+stlWriter.SetFileName(outputfilename)
+stlWriter.SetInputConnection(reader.GetOutputPort())
+stlWriter.Write()
