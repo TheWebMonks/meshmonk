1ac9c5a5f8fcbf27a760a0d4c89fe1a2389b4dd0
jsnyde0 <jonatansnyders@gmail.com>
2016-10-17 18:56:51 +0200

    First steps towards implementing symmetric, inlier-weighted correspondences

--- registration/core.py
+++ registration/core.py
@@ -24,89 +24,97 @@ from numpy import linalg
 from scipy import spatial
 import helpers
 
-def symmetric_wknn_correspondences(features1, features2, correspondingFeatures, indices12, indices21, k = 3):
+def wknn_affinity(features1, features2, weights2, indices12, affinity12, k = 3):
     """
     # GOAL
-    For each element in features1, you're going to determine corresponding
-    features by looking at the elements present in features2. This is done with
-    'symmetric weighted k nearest neighbours'. Concretely:
-    ## K Nearest Neighbours
-    The k nearest neighbours for each element in features1 are sought in
-    features2 using the Euclidean distance metric
-    ## Weighted:
-    The k nearest neighbours are combined through weighted averaging
-    to obtain a correspondence
-    ## Symmetric:
-    The final correspondences in features2 for features1 are determined by
-    combining weighted k-nn correspondences in features2 for features1 and
-    weighted k-nn correspondences in features1 for features2. This can be though
-    of as combining push and pull forces.
+    For each element in features1, you're going to determine affinity weights
+    which link it to elements in features2. This is based on the Euclidean
+    distance of k nearest neighbours found for each element of features1.
 
     # INPUTS
     -features1
     -features2
+    -weights2
 
 
     # PARAMETERS
-
+    -k(=3): number of nearest neighbours
 
     # OUTPUT
-    -correspondingFeatures
     -indices12
-    -indices21
+    -affinity12
 
     # RETURNS
     """
+    # Obtain some required info
+    numElements1 = features1.shape[0]
+    numElements2 = features2.shape[0]
+    # Determine the nearest neighbours
     helpers.nearest_neighbours(features1, features2, distances, neighbourIndices, k):
-    helpers.nearest_neighbours(features1, features2, distances, neighbourIndices, k):
-    # Construct the affinity matrices
-    ## Initialize the matrices
-    affinity12 = np.zeros((numVertices1, numVertices2), dtype = float)
-    affinity21 = np.zeros((numVertices2, numVertices1), dtype = float)
+    # Compute the affinity matrix
+    ## Initialize the matrix
+    affinity12 = np.zeros((numElements1, numElements2), dtype = float)
     ## Loop over the first feature set to determine their affinity with the
     ## second set.
     for i in range(numVertices1):
-        ### Loop over k nearest neighbours found in the second set
+        ### Loop over k nearest neighbours
         for j, idx in enumerate(indices12[i,:]):
             ### idx now indicates the index of the second set element that was
             ## determined a neighbour of the current first set element
             distance = distances12[i,j]
             if distance < 0.001:
                 distance = 0.001 #numeric stability
-            weight = 1.0/(distance * distance)
-            ### this weight should be inserted in the affinity matrix element that
+            ## The affinity is 1 over the squared distance, weighed by the
+            ## corresponding weight of the nearest neighbour (weights2)
+            affinity = weights2[idx] * 1.0/(distance * distance)
+            if affinity < 0.0001:
+                affinity = 0.0001 #numeric stability
+            ### this should be inserted in the affinity matrix element that
             ### links the first set element index i with second element set at
             ### index 'idx'
-            affinity12[i,idx] = weight
+            affinity12[i,idx] = affinity
 
-    ## Loop over the second set elements to determine their affinity with the
-    ## first set elements
-    for i in range(numVertices2):
-        ### Loop over k nearest neighbours found in the first set elements
-        for j, idx in enumerate(indices21[i,:]):
-            ### idx now indicates the index of the first set element that was
-            ### determined a neighbour of the current second set element
-            distance = distances21[i,j]
-            if distance < 0.001:
-                distance = 0.001 #numeric stability
-            weight = 1.0/(distance * distance)
-            ### This weight should be inserted in the affinity matrix element
-            ### that links the second set element at index i with the first set
-            ### element at index 'idx'
-            affinity21[i,idx] = weight
-
-    ## Normalize both affinity matrices (the sum of each row has to equal 1.0)
+    ## Normalize the affinity matrix (the sum of each row has to equal 1.0)
     rowSums = affinity12.sum(axis=1, keepdims=True)
     affinity12 = affinity12 / rowSums
-    rowSums = affinity21.sum(axis=1, keepdims=True)
-    affinity21 = affinity21 / rowSums
-    ## Sum the affinity matrices and normalize again
-    affinity12 = affinity12 + affinity21.transpose()
-    rowSums = affinity12.sum(axis=1, keepdims=True)
-    affinity12 = affinity12 / rowSums
-    ## Now we have constructed the affinity matrix using push-pull forces, let's
-    ## construct the corresponding features.
-    correspondingFeatures = affinity12.dot(targetFeatures)
+
+def fuse_affinities(affinity12, affinity21, weights1, weights2, affinityTotal):
+    """
+    # GOAL
+    Fuse the two affinity matrices together, taking weights into account.
+
+    # INPUTS
+    -affinity12
+    -affinity21: dimensions should be transposed of affinity12
+    -weights1
+    -weights2
+
+    # PARAMETERS
+    -k(=3): number of nearest neighbours
+
+    # OUTPUT
+    -affinityTotal
+
+    # RETURNS
+    """
+    # Obtain Info
+    numElements1 = affinity12.shape[0]
+    numElements2 = affinity12.shape[1]
+    # Initialize total affinity matrix
+    for i in range(numElements1):
+        w1 = weights1[i]
+        w2 = weights2[i]
+        sumWeights = w1 + w2
+        if sumWeights > 0.0001:
+            #TODO: should tranpose affinity21?
+            affinityTotal[i,:] = (w2 * affinity12[i,:] + w1 * affinity21.tranpose()[i,:]) / sumWeights
+        else:
+            affinityTotal[i,:] = np.zeros((numElements2), dtype = float)
+
+    # Normaly, the affinity matrix should be normalized now. TODO: verify this
+    #rowSums = affinityFloatingToTarget.sum(axis=1, keepdims=True)
+    #affinityFloatingToTarget = affinityFloatingToTarget / rowSums
+
 
 def inlier_detection(features, correspondingFeatures, indices12, indices21, k = 3):
     """
