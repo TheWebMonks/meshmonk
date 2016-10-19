99b9357f7524f5199b015ae9a6d0f7e8193c0a30
jsnyde0 <jonatansnyders@gmail.com>
2016-10-19 16:02:16 +0200

    Added gaussian interpolation of scalar and vector fields as helper functions. Tests are succesful.

--- registration/helpers.py
+++ registration/helpers.py
@@ -45,7 +45,7 @@ def nearest_neighbours(features1, features2, k = 3, leafsize = 15, eps = 0.0001,
     -eps(=0.0001): margin of error on distance allowed
     -p(=2): Minkowski-norm: 2 for Euclidean
     -maxDistance(=1000)
-    
+
     RETURNS
     -distances
     -neighbourIndices.
@@ -57,3 +57,106 @@ def nearest_neighbours(features1, features2, k = 3, leafsize = 15, eps = 0.0001,
     distances, neighbourIndices = kdTree.query(features1, k, eps, p, maxDistance)
 
     return distances, neighbourIndices
+
+def gaussian_vector_interpolation(position, fieldPositions, fieldVectors, fieldWeights, sigmaa):
+    """
+    GOAL
+    This function does 'Gaussian interpolation' of vectors in an irregular
+    vector field to estimate the vector of a certain position.
+
+    INPUT
+    -position:
+    The postion where you want to calculate the vector value at
+    -fieldPositions:
+    Positions of the vectors of the vector field. The dimensions of this matrix
+    are numVectors x 3
+    -fieldVectors:
+    Values of the vectors of the vector field. The dimensions of this matrix
+    are numVectors x numFeatures
+    -fieldWeights:
+    The contribution of each vector field vector can be weighed.
+
+    PARAMETERS
+    -sigmaa:
+    The sigma used in the Gaussian averaging.
+
+    RETURNS
+    -interpolatedVector:
+    Computed through Gaussian averaging, taking additional weights (see
+    fieldWeights) into account.
+    """
+    # Obtain info and initialize outputs
+    numVectors = fieldVectors.shape[0]
+    numFeatures = fieldVectors.shape[1]
+    interpolatedVector = np.zeros((numFeatures), dtype = float)
+
+    # Loop over the field vectors
+    sumWeights = 0.0
+    for i, vector in enumerate(fieldVectors):
+        ## Compute distance
+        distanceVector = fieldPositions[i] - position
+        distance = linalg.norm(distanceVector)
+        ## From the distance, we can compute the gaussian weight
+        gaussianWeight = np.exp(-0.5 * np.square(distance / sigmaa))
+        ## Let's take the user-defined weight into account
+        combinedWeight = gaussianWeight * fieldWeights[i]
+        ## Weigh the current vector field vector and sum it
+        interpolatedVector = interpolatedVector + combinedWeight * vector
+        ## Add the weight to the total sum of weights for normalization after.
+        sumWeights = sumWeights + combinedWeight
+
+    # Normalize the vector
+    interpolatedVector = interpolatedVector / sumWeights
+
+    return interpolatedVector
+
+def gaussian_scalar_interpolation(position, fieldPositions, fieldScalars, fieldWeights, sigmaa):
+    """
+    GOAL
+    This function does 'Gaussian interpolation' of scalars in an irregular
+    scalar field to estimate the scalar of a certain position.
+
+    INPUT
+    -position:
+    The postion where you want to calculate the scalar value at
+    -fieldPositions:
+    Positions of the nodes of the scalar field. The dimensions of this matrix
+    are numNodes x 3
+    -fieldScalars:
+    Scalar values of the nodes of the scalar field. The dimensions of this array
+    are numNodes x 1
+    -fieldWeights:
+    The contribution of each scalar field node can be weighed.
+
+    PARAMETERS
+    -sigmaa:
+    The sigma used in the Gaussian averaging.
+
+    RETURNS
+    -interpolatedScalar:
+    Computed through Gaussian averaging, taking additional weights (see
+    fieldWeights) into account.
+    """
+    # Obtain info and initialize outputs
+    numNodes = fieldScalars.shape[0]
+    interpolatedScalar = 0.0
+
+    # Loop over the field nodes
+    sumWeights = 0.0
+    for i, scalar in enumerate(fieldScalars):
+        ## Compute distance
+        distanceVector = fieldPositions[i] - position
+        distance = linalg.norm(distanceVector)
+        ## From the distance, we can compute the gaussian weight
+        gaussianWeight = np.exp(-0.5 * np.square(distance / sigmaa))
+        ## Let's take the user-defined weight into account
+        combinedWeight = gaussianWeight * fieldWeights[i]
+        ## Weigh the current node's scalar and sum it
+        interpolatedScalar = interpolatedScalar + combinedWeight * scalar
+        ## Add the weight to the total sum of weights for normalization after.
+        sumWeights = sumWeights + combinedWeight
+
+    # Normalize the vector
+    interpolatedScalar = interpolatedScalar / sumWeights
+
+    return interpolatedScalar
--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -13,5 +13,44 @@
    See the License for the specific language governing permissions and
    limitations under the License.
 """
+import sys
+sys.path.append('/home/jonatan/projects/OpenMesh/build/Build/python')
+import openmesh as om
+import numpy as np
+from scipy import linalg
 
-from .context import registration
+from context import registration
+
+##########
+# TEST VECTOR INTERPOLATION
+##########
+# Set up test data
+position = np.array([0.0,0.0,0.0], dtype = float)
+fieldPositions = np.array([[1.0,1.0,0.0],[1.0,-1.0,0.0],[-1.0,-1.0,0.0],[-1.0,1.0,0.0]], dtype = float)
+fieldVectors = np.array([[1.0,1.0,1.0],[1.0,-1.0,1.0],[-1.0,-1.0,1.0],[-1.0,1.0,1.0]], dtype = float)
+fieldWeights = np.ones((4), dtype = float)
+sigmaa = 1.0
+expectedResult = np.array([0.0,0.0,1.0], dtype = float)
+interpolatedVector = registration.helpers.gaussian_vector_interpolation(position, fieldPositions, fieldVectors, fieldWeights, sigmaa)
+# Check if the result equals the expected result.
+if linalg.norm(interpolatedVector - expectedResult) < 0.00001:
+    print('gaussian_vector_interpolation is successful!')
+else:
+    print('A problem has occured in gaussian_vector_interpolation!')
+
+##########
+# TEST SCALAR INTERPOLATION
+##########
+# Set up test data
+position = np.array([0.0,0.0,0.0], dtype = float)
+fieldPositions = np.array([[1.0,1.0,0.0],[1.0,-1.0,0.0],[-1.0,-1.0,0.0],[-1.0,1.0,0.0]], dtype = float)
+fieldScalars = np.array([1.0,2.0,3.0,4.0], dtype = float)
+fieldWeights = np.ones((4), dtype = float)
+sigmaa = 1.0
+expectedResult = 2.5
+interpolatedScalar = registration.helpers.gaussian_scalar_interpolation(position, fieldPositions, fieldScalars, fieldWeights, sigmaa)
+# Check if the result equals the expected result.
+if linalg.norm(interpolatedScalar - expectedResult) < 0.00001:
+    print('gaussian_scalar_interpolation is successful!')
+else:
+    print('A problem has occured in gaussian_scalar_interpolation!')
