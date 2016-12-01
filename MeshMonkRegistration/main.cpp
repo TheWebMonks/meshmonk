009d93e8c8aad81da7cd4514d32748f4220478ba
jsnyde0 <jonatansnyders@gmail.com>
2016-12-01 16:42:06 +0100

    Nanoflann's kd-tree used succesfully in finding k nearest neighbours.

--- MeshMonkRegistration/main.cpp
+++ MeshMonkRegistration/main.cpp
@@ -8,29 +8,33 @@
 
 using namespace std;
 
+const int NUM_FEATURES = 6;
+
 typedef OpenMesh::TriMesh_ArrayKernelT<>  TriMesh;
 typedef Eigen::Matrix< unsigned int, Eigen::Dynamic, Eigen::Dynamic> MatMxNi; //matrix MxN of type unsigned int
 typedef Eigen::Matrix< float, Eigen::Dynamic, Eigen::Dynamic> MatMxNf; //matrix MxN of type float
 typedef Eigen::Matrix< float, Eigen::Dynamic, 3> MatMx3f; //matrix Mx3 of type float
-typedef Eigen::Matrix< float, Eigen::Dynamic, 6> MatMx6f; //matrix Mx6 of type float
-typedef nanoflann::KDTreeEigenMatrixAdaptor<MatMx6f, 6, nanoflann::metric_L2>  NanoKDTree6D;
+typedef Eigen::Matrix< float, Eigen::Dynamic, NUM_FEATURES> MatMx6f; //matrix Mx6 of type float
+typedef nanoflann::KDTreeEigenMatrixAdaptor<MatMx6f, NUM_FEATURES, nanoflann::metric_L2>  NanoKDTree6D;
 
-void k_nearest_neighbours(const MatMx6f &inQueriedFeatures,
-                        const MatMx6f &inSourceFeatures,
+
+
+void k_nearest_neighbours(const MatMx6f &inQueriedPoints,
+                        const MatMx6f &inSourcePoints,
                         MatMxNi &outNeighbourIndices,
                         MatMxNf &outNeighbourSquaredDistances,
                         const size_t paramK = 3,
                         const size_t paramLeafsize = 15){
     /*
     GOAL
-    This function searches for the k nearest neighbours in 'inSourceFeatures' for
-    each element in the 'inQueriedFeatures' set. It outputs the indices of each
+    This function searches for the k nearest neighbours in 'inSourcePoints' for
+    each element in the 'inQueriedPoints' set. It outputs the indices of each
     neighbour and the squared (!) distances between each element of
-    'inQueriedFeatures' and its neighbours.
+    'inQueriedPoints' and its neighbours.
 
     INPUT
-    -inQueriedFeatures:
-    -inSourceFeatures:
+    -inQueriedPoints:
+    -inSourcePoints:
 
     PARAMETERS
     -paramK(= 3): number of nearest neighbours
@@ -42,17 +46,14 @@ void k_nearest_neighbours(const MatMx6f &inQueriedFeatures,
     */
 
     //# Info and Initialization
-    const size_t dimension = inSourceFeatures.cols();
-    const size_t numSourceElements = inSourceFeatures.rows();
-    const size_t numQueriedElements = inQueriedFeatures.rows();
-    cout << "dimension [" << dimension << "] - numSourceElements [" <<
-    numSourceElements << "] - numQueriedElements [" << numQueriedElements << "]"
-    << endl;
-    cout << "inQueriedFeatures block: \n" << inQueriedFeatures.block<10,6>(0,0) << endl;
-    cout << "inSourceFeatures block: \n" << inSourceFeatures.block<10,6>(0,0) << endl;
+    const size_t dimension = inSourcePoints.cols();
+    const size_t numSourceElements = inSourcePoints.rows();
+    const size_t numQueriedElements = inQueriedPoints.rows();
+    outNeighbourIndices = MatMxNi::Zero(numQueriedElements,paramK);
+    outNeighbourSquaredDistances = MatMxNf::Zero(numQueriedElements,paramK);
 
     //# Construct kd-tree
-    NanoKDTree6D kdTree(dimension, inSourceFeatures, paramLeafsize);
+    NanoKDTree6D kdTree(dimension, inSourcePoints, paramLeafsize);
     kdTree.index->buildIndex();
 
     //# Query the kd-tree
@@ -64,25 +65,23 @@ void k_nearest_neighbours(const MatMx6f &inQueriedFeatures,
     std::vector<size_t> neighbourIndices(paramK);
     std::vector<float> neighbourSquaredDistances(paramK);
     nanoflann::KNNResultSet<float> knnResultSet(paramK);
-    knnResultSet.init(&neighbourIndices[0], &neighbourSquaredDistances[0]);
 
     //### Execute loop
     for ( ; i < numQueriedElements ; ++i ) {
-        //### Copy input features to 'queriedFeature'
-        for (j = 0 ; j < dimension ; ++j) {
-            queriedFeature[j] = inQueriedFeatures(i,j);
-        }
+        //### Initiliaze the knnResultSet
+        knnResultSet.init(&neighbourIndices[0], &neighbourSquaredDistances[0]);
 
-        if (i < 3) {
-            cout << "Step " << i << ": queried feature: " << queriedFeature[0] << endl;
-            cout << "Step " << i << ": pre-knn neighbourIndices: " << neighbourIndices[0] << endl;
+        //### convert input features to 'queriedFeature' std::vector structure
+        //### (required by nanoflann's kd-tree).
+        for (j = 0 ; j < dimension ; ++j) {
+            queriedFeature[j] = inQueriedPoints(i,j);
         }
 
         //### Query the kd-tree
-        kdTree.index->findNeighbors(knnResultSet, &queriedFeature[0], nanoflann::SearchParams(32, 0.0001, true));
-        if (i < 3) {
-            cout << "Step " << i << ": post-knn neighbourIndices: " << neighbourIndices[0] << endl;
-        }
+//        size_t numNeighboursFound = kdTree.knnSearch(&queriedFeature[0], paramK, &neighbourIndices[0], &neighbourSquaredDistances[0]);
+        kdTree.index->findNeighbors(knnResultSet, &queriedFeature[0],
+                                    nanoflann::SearchParams(32, 0.0001 /*eps*/, true));
+
         //### Copy the result into the outputs by looping over the k nearest
         //### neighbours
         for (j = 0 ; j < paramK ; ++j) {
@@ -92,7 +91,10 @@ void k_nearest_neighbours(const MatMx6f &inQueriedFeatures,
     }
 }//end k_nearest_neighbours()
 
-void openmesh_to_eigen_features(const TriMesh &inputMesh, MatMx6f &outputFeatures){
+
+
+void openmesh_to_eigen_features(const TriMesh &inputMesh,
+                                MatMx6f &outputFeatures){
     /*
     GOAL
     This function converts an openmesh data structure to the feature
@@ -114,13 +116,7 @@ void openmesh_to_eigen_features(const TriMesh &inputMesh, MatMx6f &outputFeature
 
     //# Info and Initialization
     const int numVertices = inputMesh.n_vertices();
-    const int numRows = outputFeatures.rows();
-    if (numVertices != numRows) {
-    cerr << "Number of vertices does not correspond with number of columns when"
-    << " calling openmesh_to_numpy_features" << endl;
-    cerr << "numVertices: " << numVertices << endl;
-    cerr << "numRows: " << numRows << endl;
-    }
+    outputFeatures = MatMx6f::Zero(numVertices,NUM_FEATURES);
 
     //# Extract the vertex positions and normals
     unsigned int i = 0;
@@ -142,14 +138,49 @@ void openmesh_to_eigen_features(const TriMesh &inputMesh, MatMx6f &outputFeature
     }
 }
 
-void eigen_features_to_openmesh(const MatMx6f &inputFeatures, TriMesh &outputMesh){
+void load_obj_to_eigen_features(const string inObjFilename,
+                                TriMesh &outMesh,
+                                MatMx6f &outFeatureMatrix){
+    /*
+    GOAL
+    This function loads an OBJ file given by a filename and converts it to
+    OpenMesh's TriMesh type and feature matrices we use internally.
+
+    INPUT
+    -inObjFilename:
+
+    PARAMETERS
+
+    OUTPUT
+    -outMesh
+    -outFeatureMatrix
+    */
+
+    OpenMesh::IO::_OBJReader_(); //NOTE: this issue should only appear when STATICALLY linking OpenMesh
+    if (!OpenMesh::IO::read_mesh(outMesh,inObjFilename)){
+        cerr << "Read error \n";
+        exit(1);
+    };
+
+    // Update Normals
+    outMesh.request_face_normals();
+    outMesh.request_vertex_normals();
+    outMesh.update_face_normals();
+    outMesh.update_vertex_normals();
+
+    // Convert to Eigen Matrices
+    openmesh_to_eigen_features(outMesh, outFeatureMatrix);
+
+}
+
+void eigen_features_to_openmesh(const MatMx6f &inFeatures, TriMesh &outMesh){
     /*
     GOAL
     This function converts a feature representation of a mesh using Eigen
     matrices into a mesh representation using OpenMesh.
 
     INPUT
-    -inputFeatures:
+    -inFeatures:
     a numVertices x 6 Eigen dense matrix where the first three columns are made
     up of the positions of the vertices, and the last three columns the normals
     of those vertices.
@@ -157,13 +188,13 @@ void eigen_features_to_openmesh(const MatMx6f &inputFeatures, TriMesh &outputMes
     PARAMETERS
 
     OUTPUT
-    -outputMesh:
+    -outMesh:
     this has to be a mesh of openmesh's TriMesh type
     */
 
     //# Info and Initialization
-    const int numRows = inputFeatures.rows();
-    const int numVertices = outputMesh.n_vertices();
+    const int numRows = inFeatures.rows();
+    const int numVertices = outMesh.n_vertices();
      if (numRows != numVertices) {
     cerr << "Number of rows does not correspond with number of vertices when"
     << " calling eigen_features_to_openmesh()" << endl;
@@ -173,21 +204,64 @@ void eigen_features_to_openmesh(const MatMx6f &inputFeatures, TriMesh &outputMes
     TriMesh::Point updatedPosition(0.0f,0.0f,0.0f);
     TriMesh::Normal updatedNormal(0.0f,0.0f,0.0f);
     unsigned int i = 0;
-    TriMesh::VertexIter vertexIt(outputMesh.vertices_begin());
-    TriMesh::VertexIter vertexEnd(outputMesh.vertices_end());
+    TriMesh::VertexIter vertexIt(outMesh.vertices_begin());
+    TriMesh::VertexIter vertexEnd(outMesh.vertices_end());
     for ( ; vertexIt != vertexEnd ; ++i, ++vertexIt) {
         //## Get positions and normals from the feature matrix
-        updatedPosition[0] = inputFeatures(i,0);
-        updatedPosition[1] = inputFeatures(i,1);
-        updatedPosition[2] = inputFeatures(i,2);
-        updatedNormal[0] = inputFeatures(i,3);
-        updatedNormal[1] = inputFeatures(i,4);
-        updatedNormal[2] = inputFeatures(i,5);
+        updatedPosition[0] = inFeatures(i,0);
+        updatedPosition[1] = inFeatures(i,1);
+        updatedPosition[2] = inFeatures(i,2);
+        updatedNormal[0] = inFeatures(i,3);
+        updatedNormal[1] = inFeatures(i,4);
+        updatedNormal[2] = inFeatures(i,5);
         //## Insert position and normal into mesh
-        outputMesh.set_point(vertexIt,updatedPosition);
-        outputMesh.set_normal(vertexIt, updatedNormal);
+        outMesh.set_point(vertexIt,updatedPosition);
+        outMesh.set_normal(vertexIt, updatedNormal);
     }
 }
+
+void write_eigen_features_to_obj(const MatMx6f &inFeatures,
+                                TriMesh &inMesh,
+                                const string inObjFilename){
+    /*
+    GOAL
+    This function converts a feature representation of a mesh using Eigen
+    matrices into a mesh representation using OpenMesh and then writes the
+    result to an obj file.
+
+    INPUT
+    -inFeatures:
+    a numVertices x 6 Eigen dense matrix where the first three columns are made
+    up of the positions of the vertices, and the last three columns the normals
+    of those vertices.
+    -inMesh
+    -inObjFilename
+
+    PARAMETERS
+
+    OUTPUT
+
+    */
+
+    //# Info and Initialization
+    const int numRows = inFeatures.rows();
+    const int numVertices = inMesh.n_vertices();
+     if (numRows != numVertices) {
+    cerr << "Number of rows does not correspond with number of vertices when"
+    << " calling write_eigen_features_to_obj()" << endl;
+    }
+
+    //# Convert feature matrix to OpenMesh structure
+    eigen_features_to_openmesh(inFeatures, inMesh);
+
+    //# Write resulting mesh to OBJ file
+    OpenMesh::IO::_OBJWriter_();
+    if (!OpenMesh::IO::write_mesh(inMesh, inObjFilename))
+    {
+        std::cerr << "write error\n";
+        exit(1);
+    }
+}//end write_eigen_features_to_obj()
 
 void update_normals_for_altered_positions(TriMesh &ioMesh, MatMx6f &ioFeatures){
     /*
@@ -255,80 +329,43 @@ void update_normals_for_altered_positions(TriMesh &ioMesh, MatMx6f &ioFeatures){
 
 int main()
 {
-    // IO options
+    //# IO options
     OpenMesh::IO::Options readOptions;
-    // IO variables
-    TriMesh bunny;
+    //# IO variables
     TriMesh fuckedUpBunny;
+    TriMesh bunny;
+    const string fuckedUpBunnyDir = "/home/jonatan/kuleuven-algorithms/examples/data/bunny_slightly_rotated.obj";
     const string bunnyDir = "/home/jonatan/kuleuven-algorithms/examples/data/bunny90.obj";
-    const string fuckedUpBunnyDir = "/home/jonatan/kuleuven-algorithms/examples/data/bunny90.obj";
     const string fuckedUpBunnyResultDir = "/home/jonatan/kuleuven-algorithms/examples/data/fucked_up_bunny_result.obj";
 
-    // Load meshes
-    OpenMesh::IO::_OBJReader_(); //NOTE: this issue should only appear when STATICALLY linking OpenMesh
-    if (!OpenMesh::IO::read_mesh(bunny,bunnyDir)){
-        cerr << "Read error \n";
-        exit(1);
-    };
-    if (!OpenMesh::IO::read_mesh(fuckedUpBunny,fuckedUpBunnyDir)){
-        cerr << "Read error \n";
-        exit(1);
-    };
-
-    // Update normals
-    fuckedUpBunny.request_face_normals();
-    fuckedUpBunny.request_vertex_normals();
-    bunny.request_face_normals();
-    bunny.request_vertex_normals();
-    fuckedUpBunny.update_face_normals();
-    bunny.update_face_normals();
-    fuckedUpBunny.update_vertex_normals();
-    bunny.update_vertex_normals();
-
-
-    //# Convert to matrices (from Eigen library)
-    const int numFloatingVertices = fuckedUpBunny.n_vertices();
-    const int numTargetVertices = bunny.n_vertices();
-    MatMx6f floatingFeatures = MatMx6f(numFloatingVertices,6);
-    MatMx6f targetFeatures = MatMx6f(numTargetVertices,6);
-    cout << "Number floating vertices: " << numFloatingVertices << endl;
-    cout << "Number target vertices: " << numTargetVertices << endl;
-
-    //## Call conversion function
-    openmesh_to_eigen_features(fuckedUpBunny, floatingFeatures);
-    openmesh_to_eigen_features(bunny, targetFeatures);
+    //# Load meshes and convert to feature matrices
+    MatMx6f floatingFeatures;
+    MatMx6f targetFeatures;
+    load_obj_to_eigen_features(fuckedUpBunnyDir, fuckedUpBunny, floatingFeatures);
+    load_obj_to_eigen_features(bunnyDir, bunny, targetFeatures);
 
     //# Do rigid ICP
     //## Find nearest neighbours
-    const size_t k = 3;
-    MatMxNi neighbourIndices = MatMxNi::Zero(numFloatingVertices,k);
-    MatMxNf neighbourSquaredDistances = MatMxNf::Zero(numFloatingVertices,k);
+    const size_t k = 2;
+    MatMxNi neighbourIndices;
+    MatMxNf neighbourSquaredDistances;
     k_nearest_neighbours(floatingFeatures, targetFeatures, neighbourIndices, neighbourSquaredDistances, k, 15);
 
-    cout << "Post kNN: " << neighbourIndices.block<10,3>(0,0) << endl;
 
 
 
 
 
-//	cout << "knnSearch(nn="<<k<<"): \n";
-//	for (unsigned int i=0;i<k;i++) {
-//		cout << "neighbourIndices["<<i<<"]=" << neighbourIndices[i] << " neighbourSquaredDistances=" << neighbourSquaredDistances[i] << endl;
-//    }
-
-
+    //TEST: ASSIGN FIRST NEIGHBOURS AS NEW LOCATIONS
+    for (unsigned int i = 0 ; i < floatingFeatures.rows() ; i++) {
+        floatingFeatures.row(i) = targetFeatures.row(neighbourIndices(i,0));
+    }
 
     //# Do non-rigid ICP
 
-    //# Convert matrices to OpenMesh structure
-    eigen_features_to_openmesh(floatingFeatures, fuckedUpBunny);
-    //# Write resulting meshes to file
-    OpenMesh::IO::_OBJWriter_();
-    if (!OpenMesh::IO::write_mesh(fuckedUpBunny, fuckedUpBunnyResultDir))
-    {
-        std::cerr << "write error\n";
-        exit(1);
-    }
+    //# Write result to file
+    write_eigen_features_to_obj(floatingFeatures, fuckedUpBunny, fuckedUpBunnyResultDir);
+
 
     return 0;
 }
