d021eef3e0f8f585c9f6d01d8ad2b292e686ee6f
jonatan <jonatansnyders@gmail.com>
2017-02-27 13:14:47 +0100

    ScaleShifter now finds which nodes in the high sampled mesh are new, and the indices of the nodes that have mathces with the low sampled mesh.

--- MeshMonkExtreme/core/ScaleShifter.cpp
+++ MeshMonkExtreme/core/ScaleShifter.cpp
@@ -5,12 +5,20 @@ namespace registration {
 
 
 void ScaleShifter::set_input(const FeatureMat &inLowFeatures,
-                       const VecDynInt &inLowOriginalIndices;
+                       const VecDynInt &inLowOriginalIndices,
                        const VecDynInt &inHighOriginalIndices){
     _inLowFeatures = &inLowFeatures;
     _inLowOriginalIndices = &inLowOriginalIndices;
     _inHighOriginalIndices = &inHighOriginalIndices;
 
+    _numLowNodes = _inLowFeatures->rows();
+    _numHighNodes = _outHighFeatures->rows();
+
+    _numCorrespondingNodes = 0;
+    _numNewNodes = 0;
+
+    _correspondingIndexPairs.clear();
+    _newIndices.clear();
 }//end set_input()
 
 
@@ -19,117 +27,110 @@ void ScaleShifter::set_output(FeatureMat &outHighFeatures){
 }//end set_output()
 
 
-void ScaleShifter::_find_corresponding_indices(){
-    //## Put the indices into arrays
+void ScaleShifter::_find_corresponding_and_new_indices(){
+    //# Put index pairs of original and current indices into arrays
+    //## Initialize the lists
+    std::vector<std::pair<int,int>> lowIndexPairs;
+    std::vector<std::pair<int,int>> highIndexPairs;
+    //## Loop over the low sampled indices
+    for (int i = 0 ; i < _numLowNodes ; i++){
+        //## Get the original index
+        int originalIndex = (*_inLowOriginalIndices)[i];
+        //## Make a pair of the original index with its current index
+        std::pair<int,int> indexPair(originalIndex,i);
+        //## Save it into the list of index pairs.
+        lowIndexPairs.push_back(indexPair);
+    }
+    //## Loop over the high sampled indices
+    for (int i = 0 ; i < _numHighNodes ; i++){
+        //## Get the original index
+        int originalIndex = (*_inHighOriginalIndices)[i];
+        //## Make a pair of the original index with its current index
+        std::pair<int,int> indexPair(originalIndex,i);
+        //## Save it into the list of index pairs.
+        highIndexPairs.push_back(indexPair);
+    }
 
-    //## Sort the arrays
 
-    //## Find the intersect between the arrays
-}
+    //# Sort the index pair lists by the original indices
+//    struct compare_first_only {
+//        bool operator()(const std::pair<int, int>& p1, const std::pair<int, int>& p2) {
+//            return p1.first < p2.first;
+//        }
+//    };
+//    std::stable_sort(lowIndexPairs.begin(), lowIndexPairs.end(), compare_first_only());
+    std::stable_sort(lowIndexPairs.begin(), lowIndexPairs.end(), [](auto &left, auto &right) {
+        return left.first < right.first;
+    }); //This construction makes sure we only sort using the first element of the std::pair
+
+
+    //# Determine corresponding and new nodes of the high sampled mesh.
+    //##    Determine the current index pairs between the corresponding nodes of the high and low
+    //##    sampled mesh (using matches between the original indices). And determine which nodes
+    //##    of the high sampled mesh have are new (have no corresponding original index in the low
+    //##    sampled mesh).
+    //## Loop over nodes of the high mesh
+    int counterLow = 0;
+    int counterHigh = 0;
+    _correspondingIndexPairs.clear();
+    for ( ; counterHigh < _numHighNodes ; counterHigh++){
+        //## Obtain original and current indices for both meshes
+        int lowOriginalIndex = lowIndexPairs[counterLow].first;
+        int lowCurrentIndex = lowIndexPairs[counterLow].second;
+        int highOriginalIndex = highIndexPairs[counterHigh].first;
+        int highCurrentIndex = highIndexPairs[counterHigh].second;
+
+        //## if the original indices match, we have found corresponding nodes!
+        if (lowOriginalIndex == highOriginalIndex) {
+            std::pair<int,int> correspondingPair(highCurrentIndex, lowCurrentIndex);
+            _correspondingIndexPairs.push_back(correspondingPair);
+            counterLow++; //counterHigh is incremented in for-loop definition
+        }
+        //## if one index is smaller than the other, we have to increment that index
+        else if (lowOriginalIndex > highOriginalIndex) {
+            //## This node is new!
+            _newIndices.push_back(highCurrentIndex);
+            //## Move to the next pair of high indices
+            continue; //skips the rest of the for-loop
+        }
+        else if (lowOriginalIndex < highOriginalIndex) {
+            //## (!) This should never occur, it means that a node was found in the low sampled mesh that doesn't exist in the high sampled mesh.
+            std::cerr << "the original indices in the low sampled mesh should be a subset of those of the high sampled mesh. Something went wrong?" << std::endl;
+            return;
+        }
+    }
+
+    //# Save the number of corresponding and new nodes
+    _numCorrespondingNodes = _correspondingIndexPairs.size();
+    _numNewNodes = _newIndices.size();
+    //## safety check
+    if((_numCorrespondingNodes + _numNewNodes) != _numHighNodes){
+        std::cerr << "Some nodes were missed as being new or corresponding nodes in ScaleShifter." << std::endl;
+    }
+}//end _find_corresponding_and_new_indices()
+
 
 void ScaleShifter::update(){
 
     //# Build the list of corresponding indices
-    //##    (The values in this list refer to the indides of the high
-    //##    resolution mesh. The index of the list itself corresponds
-    //##    with the index of the low resolution mesh. That gives us
-    //##    a link between the vertices of the two meshes that correspond
-    //##    to each other.)
+    _find_corresponding_and_new_indices();
 
-    //## Loop over the low resolution vertices
-    size_t numLowVertices = _inLowFeatures->rows();
-    size_t numHighVertices = _outHighFeatures->rows();
-    for (size_t i = 0 ; i < numLowVertices ; i++){
-
+    //# Copy the features of corresponding nodes
+    //## Loop over the corresponding nodes
+    for (int i = 0 ; i < _numCorrespondingNodes ; i++){
+        //## Get the index pairs
+        int highIndex = _correspondingIndexPairs[i].first;
+        int lowIndex = _correspondingIndexPairs[i].second;
+        //## copy the features of the low mesh into the features of the high mesh
+        _outHighFeatures->row(highIndex) = _inLowFeatures->row(lowIndex);
     }
 
+    //# Interpolate the features of new nodes
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-    //# Convert the input data to OpenMesh's mesh structure
-//    TriMesh inMesh;
-//    convert_matrices_to_mesh(*_inFeatures,
-//                            *_inFaces,
-//                            *_inFlags,
-//                            mesh);
-//
-//
-//
-//
-//    //# Downsample using OpenMesh library
-//    //## Print Mesh property
-//    std::cout << "--------  Before processing " << std::endl;
-//    std::cout << "# Vertices " << mesh.n_vertices() << std::endl;
-//    std::cout << "# Edges " << mesh.n_edges() << std::endl;
-//    std::cout << "# Faces " << mesh.n_faces() << std::endl;
-//    //## block boundary vertices
-//    mesh.request_vertex_status();
-//    //### Get an iterator over all halfedges
-//    TriMesh::HalfedgeIter he_it, he_end=mesh.halfedges_end();
-//    //### If halfedge is boundary, lock the corresponding vertices
-//    for (he_it = mesh.halfedges_begin(); he_it != he_end ; ++he_it) {
-//      if (mesh.is_boundary(*he_it)) {
-//         mesh.status(mesh.to_vertex_handle(*he_it)).set_locked(true);
-//         mesh.status(mesh.from_vertex_handle(*he_it)).set_locked(true);
-//      }
-//    }
-//    //## Make sure mesh has necessary normals etc
-//    mesh.request_face_normals();
-//    mesh.update_face_normals();
-//
-//    //## Set up the decimator
-//    DecimaterType decimater(mesh);  // a decimater object, connected to a mesh
-//    HModQuadric hModQuadric;      // use a quadric module
-//    bool addSucces = decimater.add( hModQuadric ); // register module at the decimater
-//    std::cout << "Adding quadric modifier to decimater : " << addSucces << std::endl;
-//
-//    std::cout << decimater.module( hModQuadric ).name() << std::endl;
-//    decimater.module(hModQuadric).unset_max_err();
-//
-//    //## Initialize the decimater
-//    bool rc = decimater.initialize();
-//    std::cout  << "Decimater Initialization - Success. " << std::endl;
-//    if (!rc){
-//        std::cerr << "  initializing failed!" << std::endl;
-//        std::cerr << "  maybe no priority module or more than one were defined!" << std::endl;
-//        return;
-//    }
-//    std::cout << "Decimater Observer: " << decimater.observer() << std::endl;
-//
-//
-//
-//    //## Run the decimater
-//    size_t numVertices = mesh.n_vertices();
-//    size_t numDecimations = size_t(round(_ScaleShifteratio * numVertices));
-//    rc = decimater.decimate(numDecimations);
-//    std::cout << " Decimation Succes? -> " << rc << std::endl;
-//
-//    //## Collect garbage
-//    mesh.garbage_collection();
-//
-//    //## Print Mesh property
-//    std::cout << "--------  After processing " << std::endl;
-//    std::cout << "# Vertices " << mesh.n_vertices() << std::endl;
-//    std::cout << "# Edges " << mesh.n_edges() << std::endl;
-//    std::cout << "# Faces " << mesh.n_faces() << std::endl;
-//
-//    //# Convert the downsampled result to the output matrices
-//    convert_mesh_to_matrices(mesh, *_outFeatures, *_outFaces, *_outFlags);
-
-}
+}//end update()
 
 
 }//namespace registration
--- MeshMonkExtreme/core/ScaleShifter.hpp
+++ MeshMonkExtreme/core/ScaleShifter.hpp
@@ -4,6 +4,7 @@
 
 #include <Eigen/Dense>
 #include <iostream>
+#include <algorithm>
 #include <OpenMesh/Core/IO/MeshIO.hh>
 #include <OpenMesh/Core/Mesh/TriMesh_ArrayKernelT.hh>
 #include "../global.hpp"
@@ -11,6 +12,7 @@
 
 typedef Eigen::Vector3f Vec3Float;
 typedef Eigen::VectorXf VecDynFloat;
+typedef Eigen::VectorXi VecDynInt;
 typedef Eigen::Matrix< float, Eigen::Dynamic, registration::NUM_FEATURES> FeatureMat; //matrix Mx6 of type float
 typedef Eigen::Matrix< int, Eigen::Dynamic, 3> FacesMat;
 typedef OpenMesh::DefaultTraits MyTraits;
@@ -23,7 +25,7 @@ class ScaleShifter
     public:
 
         void set_input(const FeatureMat &inLowFeatures,
-                       const VecDynInt &inLowOriginalIndices;
+                       const VecDynInt &inLowOriginalIndices,
                        const VecDynInt &inHighOriginalIndices);
         void set_output(FeatureMat &outHighFeatures);
         void update();
@@ -43,10 +45,17 @@ class ScaleShifter
         //# User Parameters
 
         //# Internal Data structures
+        std::vector<std::pair<int,int>> _correspondingIndexPairs;
+        std::vector<int> _newIndices;
 
         //# Internal Parameters
+        size_t _numLowNodes = 0;
+        size_t _numHighNodes = 0;
+        size_t _numCorrespondingNodes = 0;
+        size_t _numNewNodes = 0;
 
         //# Internal functions
+        void _find_corresponding_and_new_indices();
 };
 
 }//namespace registration
