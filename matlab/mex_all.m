dd9fa24a4bb767fec3a4d70c4525f6234edfed45
Jonatan Snyders <jsnyde0@radmevislab.uz.kuleuven.ac.be>
2017-05-11 16:08:44 +0200

    Made mexing and compiling OSX compatible.

--- matlab/mex_all.m
+++ matlab/mex_all.m
@@ -1,20 +1,20 @@
 disp('Mexing "compute_correspondences"...')
-mex compute_correspondences.cpp -lmeshmonk
+mex -I/usr/local/include/ compute_correspondences.cpp -lmeshmonk
 disp('Mexing "compute_inlier_weights"...')
-mex compute_inlier_weights.cpp -lmeshmonk
+mex -I/usr/local/include/ compute_inlier_weights.cpp -lmeshmonk
 disp('Mexing "compute_nonrigid_transformation"...')
-mex compute_nonrigid_transformation.cpp -lmeshmonk
+mex -I/usr/local/include/ compute_nonrigid_transformation.cpp -lmeshmonk
 disp('Mexing "compute_rigid_transformation"...')
-mex compute_rigid_transformation.cpp -lmeshmonk
+mex -I/usr/local/include/ compute_rigid_transformation.cpp -lmeshmonk
 disp('Mexing "downsample_mesh"...')
-mex downsample_mesh.cpp -lmeshmonk
+mex -I/usr/local/include/ downsample_mesh.cpp -lmeshmonk
 disp('Mexing "nonrigid_registration"...')
-mex nonrigid_registration.cpp -lmeshmonk
+mex -I/usr/local/include/ nonrigid_registration.cpp -lmeshmonk
 disp('Mexing "pyramid_registration"...')
-mex pyramid_registration.cpp -lmeshmonk
+mex -I/usr/local/include/ pyramid_registration.cpp -lmeshmonk
 disp('Mexing "rigid_registration"...')
-mex rigid_registration.cpp -lmeshmonk
+mex -I/usr/local/include/ rigid_registration.cpp -lmeshmonk
 disp('Mexing "scaleshift_mesh"...')
-mex scaleshift_mesh.cpp -lmeshmonk
+mex -I/usr/local/include/ scaleshift_mesh.cpp -lmeshmonk
 disp('Mexing "compute_normals"...')
-mex compute_normals.cpp -lmeshmonk
+mex -I/usr/local/include/ compute_normals.cpp -lmeshmonk
